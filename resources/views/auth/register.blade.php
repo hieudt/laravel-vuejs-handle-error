b3c1cf50a8ad81a386378732266e8b98677f140a
hieuleader <hieuleadergin@gmail.com>
2019-12-17 19:17:41 +0700

    Feature : Error handle

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Register') }}</div>
+
+                <div class="card-body">
+                    <form @submit.prevent="submit">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" :class="{ 'is-invalid' : errors.get('name') }" v-model="fields.name">
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>@{{ errors.get('name') }}</strong>
+                                </span>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" :class="{ 'is-invalid' : errors.get('email') }" v-model="fields.email">
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>@{{ errors.get('email') }}</strong>
+                                </span>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input type="password" v-model="fields.password" class="form-control" :class="{ 'is-invalid' : errors.get('password') }">
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>@{{ errors.get('password') }} </strong>
+                                </span>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <div class="col-md-6">
+                                <input type="password" v-model="fields.password_confirmation" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
