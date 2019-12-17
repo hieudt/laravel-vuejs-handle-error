95be70bdd96d4fd82ab09e32144d370a34db34fe
hieuleader <hieuleadergin@gmail.com>
2019-12-17 19:17:41 +0700

    Feature : Error handle

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,41 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+class Errors {
+    constructor() {
+        this.errors = {};
+    }
+
+    get(field) {
+        if (this.errors[field]) {
+            return this.errors[field][0];
+        }
+    }
+
+    record(errors) {
+        this.errors = errors.errors;
+    }
+}
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        fields: {},
+        errors: new Errors(),
+        errorStatus: false,
+    },
+    methods: {
+        submit() {
+            axios.post('/register', this.fields).then(response => {
+                console.log('Success');
+            }).catch(error => {
+                if (error.response.status === 422) {
+                    this.errors.record(error.response.data) || {};
+                    this.errorStatus = true;
+                }
+            });
+        }
+    }
+});
